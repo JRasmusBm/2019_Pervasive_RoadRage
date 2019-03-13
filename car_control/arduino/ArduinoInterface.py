1d2fdf8d4ff286da8bcc4dcc0cc98ff22993f7b0
jrasmusbm <JRasmusBm@gmail.com>
2019-03-13 10:29:41 +0100

    Low medium high

--- car_control/arduino/ArduinoInterface.py
+++ car_control/arduino/ArduinoInterface.py
@@ -34,13 +34,13 @@ class ArduinoInterface:
                 if message == b"left":
                     self.arduino.write(b"l")
                     self.sent += 1
-                if message == b"0":
+                if message == b"low":
                     self.arduino.write(b"1")
                     self.sent += 1
-                if message == b"1":
+                if message == b"medium":
                     self.arduino.write(b"2")
                     self.sent += 1
-                if message == b"2":
+                if message == b"high":
                     self.arduino.write(b"3")
                     self.sent += 1
             if self.arduino.inWaiting() > 0:
